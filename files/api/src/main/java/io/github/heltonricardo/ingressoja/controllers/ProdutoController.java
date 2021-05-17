fa3eb6dc0b17cb592adbdadcd55a43a94fd498aa
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-16 21:49:06 -0300

    validacao de atributos

--- files/api/src/main/java/io/github/heltonricardo/ingressoja/controllers/ProdutoController.java
+++ files/api/src/main/java/io/github/heltonricardo/ingressoja/controllers/ProdutoController.java
@@ -1,24 +1,24 @@
 package io.github.heltonricardo.ingressoja.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.heltonricardo.ingressoja.model.entities.Produto;
 import io.github.heltonricardo.ingressoja.model.repositories.ProdutoRepository;
 
 @RestController
-@RequestMapping("api/produtos")
+@RequestMapping("/api/produtos")
 public class ProdutoController {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
 
 	@PostMapping
-	public Produto novoProduto(@RequestParam String nome) {
-		Produto produto = new Produto(nome);
+	public Produto novoProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
 	}
--- files/api/src/main/java/io/github/heltonricardo/ingressoja/model/entities/Produto.java
+++ files/api/src/main/java/io/github/heltonricardo/ingressoja/model/entities/Produto.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Produto {
@@ -12,13 +15,22 @@ public class Produto {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
+	@NotBlank
 	private String nome;
+	
+	@Min(0)
+	private double preco;
+	
+	@Min(0) @Max(1)
+	private double desconto;
 
 	public Produto() {
 	}
 
-	public Produto(String nome) {
+	public Produto(String nome, double preco, double desconto) {
 		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
 	}
 
 	public int getId() {
@@ -35,5 +47,21 @@ public class Produto {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(double desconto) {
+		this.desconto = desconto;
 	}
 }
