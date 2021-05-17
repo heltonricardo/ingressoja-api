4e813214c54fe773842b232ad544f8e30caf71f2
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-16 22:48:09 -0300

    obtencao de produtos por pagina

--- files/api/src/main/java/io/github/heltonricardo/ingressoja/model/repositories/ProdutoRepository.java
+++ files/api/src/main/java/io/github/heltonricardo/ingressoja/model/repositories/ProdutoRepository.java
@@ -1,9 +1,10 @@
 package io.github.heltonricardo.ingressoja.model.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import io.github.heltonricardo.ingressoja.model.entities.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository
+		extends PagingAndSortingRepository<Produto, Integer> {
 
 }
