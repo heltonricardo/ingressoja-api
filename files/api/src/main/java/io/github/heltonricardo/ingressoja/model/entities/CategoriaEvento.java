5d4ae4e20ea360cf76356e4563361e718ef13243
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-31 22:49:14 -0300

    get e set de categoria evento

--- files/api/src/main/java/io/github/heltonricardo/ingressoja/model/entities/CategoriaEvento.java
+++ files/api/src/main/java/io/github/heltonricardo/ingressoja/model/entities/CategoriaEvento.java
@@ -23,4 +23,28 @@ public class CategoriaEvento {
 	
 	@OneToMany
 	private List<Evento> eventos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Evento> getEventos() {
+		return eventos;
+	}
+
+	public void setEventos(List<Evento> eventos) {
+		this.eventos = eventos;
+	}
 }
