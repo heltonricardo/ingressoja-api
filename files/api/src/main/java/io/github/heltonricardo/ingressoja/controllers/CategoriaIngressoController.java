869333835f916247e251913dd3e689aa11acd06c
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-06-02 19:21:31 -0300

    apagando import desnecessario

--- files/api/src/main/java/io/github/heltonricardo/ingressoja/controllers/CategoriaIngressoController.java
+++ files/api/src/main/java/io/github/heltonricardo/ingressoja/controllers/CategoriaIngressoController.java
@@ -5,8 +5,6 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
