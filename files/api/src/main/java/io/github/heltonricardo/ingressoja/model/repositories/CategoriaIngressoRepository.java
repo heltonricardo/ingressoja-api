8a68318d3a345645ced21f98bd94525712dce664
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-06-02 21:19:30 -0300

    remocao da categoria ingresso da api

--- files/api/src/main/java/io/github/heltonricardo/ingressoja/model/repositories/CategoriaIngressoRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.heltonricardo.ingressoja.model.repositories;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import io.github.heltonricardo.ingressoja.model.entities.CategoriaIngresso;
-
-public interface CategoriaIngressoRepository
-		extends PagingAndSortingRepository<CategoriaIngresso, Long> {
-}
