1af14a06f944756e1dbfd6f8d84b5ce59e28a9d2
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-06-04 22:57:39 -0300

    pedido controller e insomnia

---
+++ files/api/src/main/java/io/github/heltonricardo/ingressoja/controllers/PedidoController.java
@@ -0,0 +1,50 @@
+package io.github.heltonricardo.ingressoja.controllers;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.heltonricardo.ingressoja.model.entities.Pedido;
+import io.github.heltonricardo.ingressoja.model.repositories.PedidoRepository;
+
+@RestController
+@RequestMapping("pedido")
+public class PedidoController {
+
+	@Autowired
+	private PedidoRepository PedidoRepository;
+
+	@GetMapping("/{id}")
+	public Optional<Pedido> obterPedidoPorId(
+			@PathVariable Long id) {
+		return PedidoRepository.findById(id);
+	}
+
+	@GetMapping
+	public Iterable<Pedido> obterPedidos() {
+		return PedidoRepository.findAll();
+	}
+
+	@GetMapping("/pagina/{numeroPagina}/{quantidade}")
+	public Iterable<Pedido> obterPedidosPorPagina(
+			@PathVariable int numeroPagina, @PathVariable int quantidade) {
+		Pageable pagina = PageRequest.of(numeroPagina, quantidade);
+		return PedidoRepository.findAll(pagina);
+	}
+
+	@PostMapping
+	public void salvarPedido(
+			@RequestBody @Valid Pedido Pedido) {
+		PedidoRepository.save(Pedido);
+	}
+}
