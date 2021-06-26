24d341c6238225efbefb47e980ff263e07d8f06f
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2021-06-25 23:41:04 -0300

    retornando status http na autenticacao

--- src/main/java/io/github/heltonricardo/ingressoja/controllers/AutenticacaoController.java
+++ src/main/java/io/github/heltonricardo/ingressoja/controllers/AutenticacaoController.java
@@ -1,6 +1,8 @@
 package io.github.heltonricardo.ingressoja.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,34 +30,44 @@ public class AutenticacaoController {
 	private OrganizadoraRepository organizadoraRepository;
 
 	@PostMapping
-	public Integer logar(@RequestBody ObjectNode obj) {
+	public ResponseEntity<?> logar(@RequestBody ObjectNode obj) {
 		String email;
 		String senha;
 		try {
 			email = obj.get("email").asText();
 			senha = obj.get("senha").asText();
 		} catch (Exception e) {
-			return 0;
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 		}
 
 		Iterable<Administrador> user1 = administradorRepository.findByEmail(email);
 		if (user1.iterator().hasNext()) {
 			Administrador administrador = user1.iterator().next();
-			return (administrador.getUsuario().getSenha().equals(senha)) ? 1 : 0;
+			if (administrador.getUsuario().getSenha().equals(senha))
+				return ResponseEntity.status(HttpStatus.OK).body(1);
+			else
+				return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 		}
 
 		Iterable<Comprador> user2 = compradorRepository.findByEmail(email);
 		if (user2.iterator().hasNext()) {
 			Comprador comprador = user2.iterator().next();
-			return (comprador.getUsuario().getSenha().equals(senha)) ? 2 : 0;
+			if (comprador.getUsuario().getSenha().equals(senha))
+				return ResponseEntity.status(HttpStatus.OK).body(2);
+			else
+				
+				return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 		}
 
 		Iterable<Organizadora> user3 = organizadoraRepository.findByEmail(email);
 		if (user3.iterator().hasNext()) {
 			Organizadora organizadora = user3.iterator().next();
-			return (organizadora.getUsuario().getSenha().equals(senha)) ? 3 : 0;
+			if (organizadora.getUsuario().getSenha().equals(senha))
+				return ResponseEntity.status(HttpStatus.OK).body(3);
+			else
+				return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 		}
 
-		return 0;
+		return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 	}
 }
