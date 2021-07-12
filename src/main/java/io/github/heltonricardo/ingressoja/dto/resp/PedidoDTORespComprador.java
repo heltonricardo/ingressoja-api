c357b4fa0bfc2d1312cd312e627851faeaf2df0f
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-07-12 16:46:49 -0300

    enviando titulo do evento para pedido

--- src/main/java/io/github/heltonricardo/ingressoja/dto/resp/PedidoDTORespComprador.java
+++ src/main/java/io/github/heltonricardo/ingressoja/dto/resp/PedidoDTORespComprador.java
@@ -14,9 +14,12 @@ public class PedidoDTORespComprador {
   private final Long id;
   private final Date dataHora;
   private final Double valorTotal;
+  private final String tituloEvento;
 
   public static PedidoDTORespComprador paraDTO(Pedido pedido) {
     return new PedidoDTORespComprador(
-        pedido.getId(), pedido.getDataHora(), pedido.getValorTotal());
+        pedido.getId(), pedido.getDataHora(), pedido.getValorTotal(),
+        pedido.getItensPedido().get(0).getTipoDeIngresso().getEvento()
+            .getTitulo());
   }
 }
