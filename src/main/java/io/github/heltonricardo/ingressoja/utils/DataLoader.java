d1d7b5e175cf991bfb3d2da4193c757a22631b43
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-07-23 16:28:29 -0300

    carregando automaticamente dados do primeiro administrador

---
+++ src/main/java/io/github/heltonricardo/ingressoja/utils/DataLoader.java
@@ -0,0 +1,26 @@
+package io.github.heltonricardo.ingressoja.utils;
+
+import io.github.heltonricardo.ingressoja.model.Administrador;
+import io.github.heltonricardo.ingressoja.model.Usuario;
+import io.github.heltonricardo.ingressoja.service.AdministradorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+  private final AdministradorService administradorService;
+
+  @Autowired
+  public DataLoader(AdministradorService administradorService) {
+    this.administradorService = administradorService;
+  }
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    administradorService.salvar(new Administrador("Administrador Padrão",
+        "a", new Usuario("a")));
+  }
+}
