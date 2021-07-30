77c60f6843937a13c5fe76268bddfbb13ea10bcc
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-07-29 21:52:34 -0300

    validando dados de categoria de evento

--- src/main/java/io/github/heltonricardo/ingressoja/dto_in/CategoriaEventoDTO.java
+++ src/main/java/io/github/heltonricardo/ingressoja/dto_in/CategoriaEventoDTO.java
@@ -3,9 +3,14 @@ package io.github.heltonricardo.ingressoja.dto_in;
 import io.github.heltonricardo.ingressoja.model.CategoriaEvento;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 public class CategoriaEventoDTO {
 
+  @NotBlank
+  @Size(max = 50)
   private String nome;
 
   public CategoriaEvento paraObjeto() {
