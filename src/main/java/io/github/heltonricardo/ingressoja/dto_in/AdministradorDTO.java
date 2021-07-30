205881be6a4e6806ff8e5c3fcc23f9804fd6be37
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-07-29 21:52:24 -0300

    validando dados de administrador

--- src/main/java/io/github/heltonricardo/ingressoja/dto_in/AdministradorDTO.java
+++ src/main/java/io/github/heltonricardo/ingressoja/dto_in/AdministradorDTO.java
@@ -3,11 +3,22 @@ package io.github.heltonricardo.ingressoja.dto_in;
 import io.github.heltonricardo.ingressoja.model.Administrador;
 import lombok.Getter;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 public class AdministradorDTO {
 
+  @NotBlank
+  @Size(max = 255)
   private String nome;
+
+  @Email
   private String email;
+
+  @Valid
   private UsuarioDTO usuario;
 
   public Administrador paraObjeto() {
