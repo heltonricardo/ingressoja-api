d0bce7098cfa73cfc36e812bc31b5c2ad896ebfa
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-11-25 22:51:15 -0300

    retornando cpf e valor de taxa do pedido analise

--- src/main/java/io/github/heltonricardo/ingressoja/dto_out/PedidoDTORespAnalise.java
+++ src/main/java/io/github/heltonricardo/ingressoja/dto_out/PedidoDTORespAnalise.java
@@ -5,23 +5,26 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class PedidoDTORespAnalise {
 
   private final Long id;
+  private final String cpf;
   private final Date dataHora;
   private final Double valorTotal;
+  private final Double taxaPlataforma;
 
   public static PedidoDTORespAnalise paraDTO(Pedido pedido) {
 
     return new PedidoDTORespAnalise(
         pedido.getId(),
+        pedido.getComprador().getCpf(),
         pedido.getDataHora(),
-        pedido.getValorTotal());
+        pedido.getValorTotal(),
+        pedido.calcularTaxaPlataforma()
+    );
   }
 }
